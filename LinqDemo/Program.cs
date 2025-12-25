2617bb42da80ff56eae049fab8ff818babcc74aa
Aaron Tanguma <the_guma@yahoo.com>
2025-12-25 07:05:47 -0700

    created solutions folder, created a list, created a variable to hold filtered list of numbers

---
+++ LinqDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+Console.WriteLine("int collections");
+
+var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//Filtering with LINQ
+var numbersAboveFive = numbers.Where(number => number > 5);
